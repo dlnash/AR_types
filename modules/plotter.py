a830f009dfdbaade1ca0ae4bf863efc8089b03f3
dlnash <dlnash@ucsb.edu>
2021-03-12 10:42:41 -0800

    add notebooks on IVT merid and zonal analysis

--- modules/plotter.py
+++ modules/plotter.py
@@ -136,10 +136,10 @@ def add_subregion_boxes(ax, subregion_xy, width, height, ecolor, datacrs):
         
     return ax
 
-def add_hasia_labels(ax, fntsize=7):        
+def add_hasia_labels(ax, fntsize=7, txtcol='white'):        
     # Add text to highlight locations.
-    txtbx = dict(fill=True, edgecolor='black', linewidth=0,alpha=0.3, zorder=101)
-    kws = {'color': 'white', 'fontsize': fntsize, 'bbox': txtbx, 'ha': 'left', 'va': 'bottom'}
+    txtbx = dict(fill=True, edgecolor='black', linewidth=0,alpha=0.8, zorder=101)
+    kws = {'color': txtcol, 'fontsize': fntsize, 'bbox': txtbx, 'ha': 'left', 'va': 'bottom'}
     xcoords = [68.00, 75.42, 75.00, 75.00, 81.09, 89.82, 71.03]
     ycoords = [35.78, 41.16, 35.69, 32.50, 29.00, 27.87, 38.35]
     txtlbl = ['Hindu Kush', 'Tien Shan', 'Karakoram', 'Western Himalaya', 'Central Himalaya', 'Eastern Himalaya', 'Pamirs']
